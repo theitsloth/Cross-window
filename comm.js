bd4bf05dd25b00aca49a35b8828ceaf21b54f3ed
Lorinc Bethlenfalvy <lorinc11@gmail.com>
2019-02-25 14:14:42 +0100

    De-stating
    Redesigned to minimize internal state

--- comm.js
+++ comm.js
@@ -1,258 +1,173 @@
-DEBUG = false;
-function Comm() {
-	//#region state containers
-	this.protocol = "SimpleCWCv0";
-	this.pending = [];
-	this.subs = []; // {channel:string, id:int, handler:(msg => undefined)}
-	this.requestHandler = () => {
-		throw new Error("Registered but no handler set!");
-	};
-	this.name = undefined;
-	this.secret = undefined;
-	//#endregion
 
-	//#region private
-	receiveMessage = (event) => {
-		var msg = event.data;
-		var sender = event.source;
-		if (DEBUG) console.log("Received", msg);
-		if (typeof msg !== "object" ||
-			msg.protocol !== this.protocol ||
-			msg.phase !== "client") return;
-		if (typeof this.pending[msg.messageId] === "function")
-			this.pending[msg.messageId].call(this, msg);
-		// Handling sub messages
-		if (msg.type == "pub") {
-			subs.find(x => x.id === msg.listener).handler(data);
-		}
-		// Handling requests
-		if (msg.type == "req" && msg.target === this.name) {
-			handleRequest(msg).then(x => {
-				send({
-					type: "res",
-					to: msg.replyCode,
-					data: {
-						error: false,
-						value: x
-					}
-				}, rec => {
-					if (rec.type !== "receipt") 
-						throw new Error("Fatal error on reply!");
-				});
-			}).catch(x => {
-				send({
-					type: "res",
-					to: msg.replyCode,
-					data: {
-						error: true,
-						value: x
-					}
-				}, rec => {
-					if (rec.type !== "receipt")
-						throw new Error("Fatal error on reply!");
-				});
-			});
-		}
-	};
-	handleRequest = async (msg) => {
-		var ret;
-		try {
-			ret = this.requestHandler(msg.data);
-			if (ret instanceof Promise)
-				return await ret;
-			else return ret;
-		} catch(e) {
-			throw e;
-		}
-	}
+// ##### Package header
+(function(stdlib){
+	var exports = {};
+	
+	//#region Definitions
+	const protocol = "SimpleCWCv0";
+
+	// Wrapper for listeners
+	getListener = (id, handler, isPersistent = false) => {
+		if (typeof handler !== "function")
+			throw new TypeError("handler is not a function!");
+		var ret = (e => {
+			msg = e.data;
+			if (typeof msg !== "object" ||
+				msg.protocol !== this.protocol ||
+				msg.phase !== "client" ||
+				(id !== undefined && msg.messageId !== id)) return;
+			// Delete self if persistence wasn't required
+			if (!isPersistent) 
+				window.removeEventListener("message", ret, false);
+			// Call the defined handler and return its return value
+			/** @todo Decide thisArg */
+			return handler.call(window, e);
+		});
+		return ret;
+	} 
+
 	send = (data, callback) => {
+		if (typeof data !== "object")
+			throw new TypeError("Data is not an object!");
+		if (typeof callback !== "function")
+			throw new TypeError("Callback is not a function!");
 		var id = Comm.generateId();
-		this.pending[id] = callback;
+		var listener = getListener(id, callback);
+		window.addEventListener("message", listener, false);
 		data.messageId = id;
 		data.protocol = this.protocol;
 		data.phase = "server";
-		Comm.getTopWindow().postMessage(data, "*");
+		getTopWindow().postMessage(data, "*");
+		return listener;
 	}
-	//#endregion private
-	//#region public
-	//#region reg/unreg
-	this.register = (name = undefined) => new Promise((resolve, reject) => {
-		if (name === undefined) name = Comm.generateId();
-		var msg = {
-			type: "reg",
-			name: name,
+
+	/**
+	 * Listen on a given name with a given function
+	 * @param {string | number | boolean} name The name of the service 
+	 * @param {function} requestHandler The request handler
+	 * @returns {Promise<{handler: function, name: string | number | boolean, destroy: function}>}
+	 * 
+	 * Listener
+	 */
+	listen = (name, requestHandler) => new Promise((res, rej) => {
+		if (name === undefined) name = generateId();
+		// Define locals
+		if (typeof name !== "string" && 
+			typeof name !== "number" &&
+			typeof name !== "boolean")
+			throw new TypeError("Name is not a primitive");
+		const _name = name;
+		var _secret = null;
+		var _handler = null;
+		// 0: preparing, 1: active, 2: destroyed
+		var _status = 0;
+		// Define object
+		var listener = {
+			// Handler is a property, it can be assumed to be a function
+			get handler() {
+				return _handler;
+			},
+			set handler(val) {
+				if (typeof val !== "function")
+					throw new TypeError("handler must be a function!");
+				_handler = val;
+			},
+			// name and destroy are constants.
+			get name() {
+				return _name;
+			},
+			get destroy() {
+				return () => new Promise((resolve, reject) => {
+					send({
+						type: "unreg",
+						secret: _secret,
+					}, res => {
+						if (res.success) {
+							window.removeEventListener("message", delegate);
+							_status = 2;
+							_secret = undefined;
+							resolve();
+						}
+						else reject(res);
+					});
+				});
+			}
 		};
-		send(msg, res => {
-			if (res.error) reject(res);
+		// Use our new made setter
+		try { listener.handler = requestHandler; }
+		catch(e) { rej(e); }
+		// Register
+		send({
+			type: "reg",
+			name: _name,
+		}, response => {
+			if (response.error) rej(response);
 			else {
-				this.name = name;
-				this.secret = res.secret;
-				resolve();
+				_secret = response.secret;
+				_status = 1;
+				window.addEventListener("message", delegate);
+				res(listener);
 			}
 		});
+		// Construct delegate
+		var delegate = getListener(undefined, async ev => {
+			var msg = ev.data;
+			// If it isn't for us or we aren't listening, return
+			if (msg.target !== name || _status !== 1) return;
+			var replyData = null;
+			try {
+				// Call our handler (catch if it throws)
+				/** @todo define "this" */
+				var handlerReturnValue = ret.handler.call(window, msg.data);
+				if (hendlerReturnValue instanceof Promise) {
+					handlerReturnValue = await handlerReturnValue;
+				}
+				replyData = {
+					error: false,
+					value: handlerReturnValue,
+				};
+			} catch(ex) {
+				if (ex instanceof Error) {
+					throw new Error(
+					"Throwing Error objects over message is not supported. "+
+					"Check your code for internal errors.");
+				}
+				replyData = {
+					error: true,
+					value: ex,
+				};
+			}
+			send({
+				type: "res",
+				to: msg.replyCode,
+				data: replyData,
+			}, receipt => {
+				if (receipt.type !== "receipt") 
+					throw new Error("Fatal error on reply!");
+			});
+		}, true);
 	});
-	this.unregister = () => new Promise((resolve, reject) => {
-		if (this.name === undefined) 
-			reject(new Error("Not registered yet."));
-		var msg = {
-			type: "unreg",
-			secret: this.secret,
-		};
-		send(msg, res => {
-			if (res.success) resolve();
-			else reject(res);
-		})
-	});
-	//#endregion reg/unreg
-	//#region req/res
-	this.onRequest = (handler) => {
-		if (typeof handler !== "function")
-			throw new Error("The handler doesn't seem to be \
-							a function.");
-		this.requestHandler = handler;
-	}
-	this.request = (name, data) => new Promise((resolve, reject) => {
+
+	request = (name, data) => new Promise((res, rej) => {
 		var msg = {
 			type: "req",
 			target: name,
 			data: data,
 		};
 		send(msg, res => {
-			if (res.error) reject(res);
-			else if (res.data && res.data.error) reject(res.data.value);
-			else resolve(res.data.value);
+			if (res.error || typeof res.data !== "object") rej(res);
+			else if (res.data && res.data.error) rej(res.data.value);
+			else res(res.data.value);
 		});
 	});
-	//#endregion req/res
-	//#region pub/sub
-	this.publish = (channel, data) => new Promise((resolve, reject) => {
-		var msg = {
-			type: "pub",
-			channel,
-			data,
-		};
-		send(msg, res => {
-			if (res.error) reject(res);
-			else resolve(res.clients);
-		});
-	});
-	this.subscribe = (channel, handler) => new Promise((resolve, reject) => {
-		if (typeof handler !== "function")
-			reject(new Error("Handler must be a function!"));
-		var msg = {
-			type: "sub",
-			channel,
-		};
-		send(msg, res => {
-			if (res.error) reject(res);
-			else {
-				subs.push({
-					channel: channel,
-					id: res.listenerId,
-					handler: handler,
-				});
-				resolve(res.clients);
-			}
-		})
-	});
-	this.unsubscribe = (channel) => new Promise((resolve, reject) => {
-		var id = subs.find(x => x.channel === channel).id;
-		var msg = {
-			type: "unsub",
-			id: id,
-		};
-		send(msg, res => {
-			if (res.error) reject(res);
-			else if (res.success) {
-				resolve(res.success);
-				subs = subs.filter(x => x.id !== id);
-			}
-		});
-	});
-	//#endregion pub/sub
-	//#region get/set
-	this.get = (name) => new Promise((resolve, reject) => {
-		var msg = {
-			type: "get",
-			name: name,
-		};
-		send(msg, res => {
-			if (res.error) reject(res);
-			else resolve(res.value);
-		});
-	});
-	this.set = (name, value) => new Promise((resolve, reject) => {
-		var msg = {
-			type: "set",
-			name: name,
-			value: value,
-		};
-		send(msg, res => {
-			if (res.error) reject(res);
-			else resolve(res.previous);
-		});
-	});
-	//#endregion get/set
-	//#region add/del/qry
-	this.addTo = (name) => new Promise((resolve, reject) => {
-		var msg = {
-			type: "add",
-			secret: this.secret,
-			name,
-		};
-		send(msg, res => {
-			if (res.error) reject(res);
-			else resolve(res.list);
- 		});
-	});
-	this.delFrom = (name) => new Promise((resolve, reject) => {
-		var msg = {
-			type: "del",
-			secret: this.secret,
-			name,
-		};
-		send(msg, res => {
-			if (res.error) reject(res);
-			else resolve(res.list);
-		});
-	});
-	this.query = (name) => new Promise((resolve, reject) => {
-		var msg = { type: "qry", name };
-		send(msg, res => {
-			if (res.error) reject(res);
-			else resolve(res.list);
-		});
-	});
-	//#endregion add/del/qry
-	//#endregion public
-	window.addEventListener("message", receiveMessage, false);
-}
 
-//#region static
-Comm.generateId = function() {
-	return Math.floor(Math.random() * Math.pow(10, 10));
-}
-Comm.getTopWindow = function() {
-	var cur = window.self;
-	// change to the local top window
-	cur = cur.top;
-	// If that is a popup
-	while (cur.opener !== null) {
-		// change to its opener's root window
-		cur = cur.opener.top;
-	}
-	// When it's the original, return it
-	return cur;
-};
-//#endregion
+	//#endregion Definitions
+	//#region Exports
+	exports.protocol = protocol;
+	exports.Listen = Listen;
 
-async function Once(name) {
-	var client = new Comm();
-	await client.register(name);
-	return await new Promise((res, rej) =>
-	client.onRequest(async m => {
-		await client.unregister()
-			.except(e => rej(e));
-		res(m);
-	}));	
-}
+	//#endregion Exports
+	// ##### Package footer
+	if (stdlib.Comm !== undefined) console.error("Multiple Comm instances!");
+	stdlib.Comm = exports;
+})(window);
